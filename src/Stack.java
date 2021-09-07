527f017c032d566c7b9a3cf119f5624caba6e913
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-09-07 14:00:58 -0400

    Basic Stack class added

--- src/Stack.java
+++ src/Stack.java
@@ -1,4 +1,39 @@
 
-public class Stack {
+/*
+ * Stack class
+ */
 
+public class Stack {
+	
+	private static class Node {
+		private int data;
+		private Node next;
+		
+		private Node(int data) {
+			this.data = data;
+		}
+	}
+	
+	private Node top;
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int peek() {
+		return top.data;
+	}
+	
+	public void push(int data) {
+		Node newTopNode = new Node(data);
+		newTopNode.next = top;
+		top = newTopNode;
+	}
+	
+	public int pop() {
+		int topData = top.data;
+		top = top.next;
+		return topData;
+	}
+	
 }
